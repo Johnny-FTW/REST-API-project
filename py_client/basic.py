030b96630daa47eb925f8f3d0de94bc5d87da3bd
Jan Hatapka <jan.hatapka@gmail.com>
2023-06-13 21:50:32 +0200

    api view generics

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, json={"product_id":"hiii"})
+response = requests.post(endpoint, json={"title": "hii" })
 
-# print(response.text)
+
 print(response.json())
-# print(response.status_code)
