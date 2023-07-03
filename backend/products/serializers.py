eff88318a38f0ee3ef83e2ed5f878010e6ac2265
Jan Hatapka <jan.hatapka@gmail.com>
2023-07-03 20:28:10 +0200

    serializer validators

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -4,12 +4,15 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from .models import Product
+from .validators import validate_title_no_hello, unique_product_title
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+    # name = serializers.CharField(source='title', read_only=True)
     class Meta:
         model = Product
         fields = [
@@ -17,11 +20,20 @@ class ProductSerializer(serializers.ModelSerializer):
             'edit_url',
             'pk',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
+
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError("title already exists")
+    #     return value
 
     # def create(self, validated_data):
     #     # return Product.objects.create(**validated_data)
---
+++ backend/products/validators.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from products.models import Product
+from rest_framework.validators import UniqueValidator
+
+
+# def validate_title(value):
+#     qs = Product.objects.filter(title__iexact=value)
+#     if qs.exists():
+#         raise serializers.ValidationError("title already exists")
+#     return value
+
+
+def validate_title_no_hello(value):
+    if "hello" in value.lower():
+        raise serializers.ValidationError("hello not allowed")
+    return value
+
+
+unique_product_title = UniqueValidator(queryset=Product.objects.all())
